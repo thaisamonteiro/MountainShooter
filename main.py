32147e195bec713beff38d6a85b48406ecb0f613
Thaisa Regina Monteiro <thaissa.monteiro13@gmail.com>
2025-03-18 14:34:25 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import pygame
+
+print('setup Start')
+pygame.init()
+window = pygame.display.set_mode(size = (600, 480))
+print('setup End')
+
+print('Loop Start')
+while True:
+    # check for all events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit() # Clse windom
+            quit() # End pygame

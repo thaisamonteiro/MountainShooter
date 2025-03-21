63ab70172bf39021adc22916d98f0a8120971bec
Thaisa Monteiro <108832100+thaisamonteiro@users.noreply.github.com>
2025-03-21 15:33:12 -0300

    Update main.py

--- main.py
+++ main.py
@@ -10,5 +10,6 @@ while True:
     # check for all events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
+            print('quiti...')
             pygame.quit() # Clse windom
             quit() # End pygame
